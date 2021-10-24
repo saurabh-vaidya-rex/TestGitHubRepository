f5a2f882c20b23b45d92aa4b01e287e866c80d01
Regional Express <saurabh.vaidya@rex.com.au>
2021-10-24 15:30:02 +1100

    i have inserted new search table

---
+++ TestGitHubRepository/SearchTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchTableViewController.swift
+//  TestGitHubRepository
+//
+//  Created by Saurabh Vaidya on 24/10/21.
+//
+
+import UIKit
+
+class SearchTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
